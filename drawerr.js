bff99fb4bf45a0429d1c124cee219ec413125405
zirwa <ztariq@oswego.edu>
2023-05-15 16:05:51 -0400

    errror fixed

--- drawerr.js
+++ drawerr.js
@@ -4,8 +4,8 @@ document.getElementById("form1").addEventListener("submit", function(event) {
   var email = document.getElementById("email1").value;
   // Perform form submission handling or further processing here
   console.log("Form 1 submitted");
-  console.log("Name: " + name);
-  console.log("Email: " + email);
+  console.log("Name1: " + name);
+  console.log("Email1: " + email);
   // You can replace the console.log statements with your desired functionality
 });
 
@@ -15,7 +15,7 @@ document.getElementById("form2").addEventListener("submit", function(event) {
   var email = document.getElementById("email2").value;
   // Perform form submission handling or further processing here
   console.log("Form 2 submitted");
-  console.log("Name: " + name);
-  console.log("Email: " + email);
+  console.log("Name2: " + name);
+  console.log("Email2: " + email);
   // You can replace the console.log statements with your desired functionality
 });
